fa91ff853af2fbda93a0186680868a03423cf239
root <root@ip-172-31-89-183.ec2.internal>
2025-07-12 14:51:49 +0000

    g

--- Day-5-statefilebackend/main.tf
+++ Day-5-statefilebackend/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "name" {
     tags = {
       Name = "state"
     }
-    
-  
+
+}  
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "test"
+  }
 }
